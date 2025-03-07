f753099d94148376de129756c6579be562fd4291
NickSager <121263610+NickSager@users.noreply.github.com>
2025-03-07 15:03:15 -0800

    Adjust fzf function in zshrc

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -193,7 +193,7 @@ let name = "Nick Sager";
 
         case "$command" in
           cd)           fzf --preview 'eza --tree --color=always {} | head -200' "$@" ;;
-          export|unset) fzf --preview "eval 'echo ${}'"         "$@" ;;
+          export|unset) fzf --preview 'echo {}' "$@" ;;
           ssh)          fzf --preview 'dig {}'                   "$@" ;;
           *)            fzf --preview "$show_file_or_dir_preview" "$@" ;;
         esac
