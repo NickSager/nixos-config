bb2ae067af908cf05345f31c5c0b0e40eff59408
NickSager <121263610+NickSager@users.noreply.github.com>
2025-04-06 15:31:20 -0700

    Add git ssh config back

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -321,19 +321,19 @@ let name = "Nick Sager";
         "/Users/${user}/.ssh/config_external"
       )
     ];
-    # matchBlocks = {
-    #   "github.com" = {
-    #     identitiesOnly = true;
-    #     identityFile = [
-    #       (lib.mkIf pkgs.stdenv.hostPlatform.isLinux
-    #         "/home/${user}/.ssh/id_github"
-    #       )
-    #       (lib.mkIf pkgs.stdenv.hostPlatform.isDarwin
-    #         "/Users/${user}/.ssh/id_github"
-    #       )
-    #     ];
-    #   };
-    # };
+    matchBlocks = {
+      "github.com" = {
+        identitiesOnly = true;
+        identityFile = [
+          (lib.mkIf pkgs.stdenv.hostPlatform.isLinux
+            "/home/${user}/.ssh/id_github"
+          )
+          (lib.mkIf pkgs.stdenv.hostPlatform.isDarwin
+            "/Users/${user}/.ssh/id_github"
+          )
+        ];
+      };
+    };
   };
 
   tmux = {
