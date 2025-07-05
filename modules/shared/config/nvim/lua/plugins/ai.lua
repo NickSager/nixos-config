6abbf160096411b18a1ba672cce6e6825c2b388f
NickSager <121263610+NickSager@users.noreply.github.com>
2025-07-05 16:15:33 -0700

    Update alacritty config - default ollama

--- modules/shared/config/nvim/lua/plugins/ai.lua
+++ modules/shared/config/nvim/lua/plugins/ai.lua
@@ -2,55 +2,37 @@ return {
   {
     "yetone/avante.nvim",
     event = "VeryLazy",
-    lazy = false,
-    version = false, -- set this if you want to always pull the latest change
+    version = false, -- Never set this value to "*"! Never!
+    ---@module 'avante'
+    ---@type avante.Config
     opts = {
       -- add any opts here
-      provider = "bedrock",
-      auto_suggestions_provider = "ollama", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
-      cursor_applying_provider = 'bedrock',
-      ollama = {
-        endpoint = "http://127.0.0.1:11434", -- Note that there is no /v1 at the end.
-        model = "qwq:32b",
+      -- for example
+      provider = "ollama",
+      auto_suggestions_provider = "ollama",
+      cursor_applying_provider = 'ollama',
+      providers = {
+        -- claude = {
+        --   endpoint = "https://api.anthropic.com",
+        --   model = "claude-sonnet-4-20250514",
+        --   timeout = 30000, -- Timeout in milliseconds
+        --     extra_request_body = {
+        --       temperature = 0.75,
+        --       max_tokens = 20480,
+        --     },
+        -- },
+        -- ollama = {
+        --   endpoint = "http://localhost:11434",
+        --   model = "qwq:32b",
+        -- },
       },
-      bedrock = {
-          model = "us.anthropic.claude-3-7-sonnet-20250219-v1:0",
-      },
-      --- To Customize These:
-      -- openai = {
-      --   endpoint = "https://api.openai.com/v1",
-      --   model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-      --   timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-      --   temperature = 0,
-      --   max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-      --   --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-      -- },
-      ---claude = {
-      ---  endpoint = "https://api.anthropic.com",
-      ---  model = "claude-3-5-sonnet-20241022",
-      ---  timeout = 30000, -- Timeout in milliseconds
-      ---  temperature = 0,
-      ---  max_tokens = 8000,
-      ---  disable_tools = true,
-      ---  ["local"] = false,
-      ---},
       behaviour = {
         enable_cursor_planning_mode = true,
-        enable_claude_text_editor_tool_mode = false, -- only works with claude provider
         auto_suggestions = false, -- Experimental stage
-        auto_set_highlight_group = true,
-        auto_set_keymaps = true,
-        auto_apply_diff_after_generation = false,
-        support_paste_from_clipboard = false,
       },
       hints = { enabled = false },
     },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
     dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       --- The below dependencies are optional,
@@ -58,6 +40,8 @@ return {
       "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
       "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
       "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      "stevearc/dressing.nvim", -- for input provider dressing
+      "folke/snacks.nvim", -- for input provider snacks
       "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
       "zbirenbaum/copilot.lua", -- for providers='copilot'
       {
