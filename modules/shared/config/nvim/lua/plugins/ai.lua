c9293259ebb5e25339b776bcb088e19a56f28be4
NickSager <sager.nick@gmail.com>
2025-10-12 19:20:29 -0700

    Update claude models

--- modules/shared/config/nvim/lua/plugins/ai.lua
+++ modules/shared/config/nvim/lua/plugins/ai.lua
@@ -177,8 +177,10 @@ return {
             schema = {
               model = {
                 mapping = 'temp',
-                default = 'us.anthropic.claude-3-7-sonnet-20250219-v1:0',
+                default = 'us.anthropic.claude-sonnet-4-5-20250929-v1:0',
                 choices = {
+                  ['us.anthropic.claude-opus-4-1-20250805-v1:0'] = { opts = { can_reason = true, has_vision = true } },
+                  ['us.anthropic.claude-sonnet-4-5-20250929-v1:0'] = { opts = { can_reason = true, has_vision = true } },
                   ['us.anthropic.claude-sonnet-4-20250514-v1:0'] = { opts = { can_reason = true, has_vision = true } },
                   ['us.anthropic.claude-3-7-sonnet-20250219-v1:0'] = { opts = { can_reason = true, has_vision = true } },
                 },
@@ -254,8 +256,7 @@ return {
           },
         },
         diff = {
-          provider = "default",
-          -- provider = "mini_diff",
+          provider = "mini_diff",
         },
       },
       opts = {
